e69ba0d01826d6307937219cbb4f0392aa85f5c4
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-08-03 19:53:34 -0500

    feat: made Item utility functions return immutable objects

--- src/shared/features/inventory/data/armor/index.ts
+++ src/shared/features/inventory/data/armor/index.ts
@@ -3,15 +3,15 @@ import { Armor } from '../../types';
 
 // Flags
 let isInitialized: boolean = false;
-const armors: Armor[] = [];
+const armors: Readonly<Armor>[] = [];
 
-export function getArmor(): Armor[] {
+export function getArmor(): ReadonlyArray<Readonly<Armor>> {
 	if (!isInitialized) {
 		const modules = getDescendantsOfType(script, 'ModuleScript');
 		for (const module of modules) {
 			try {
 				// biome-ignore lint/style/noCommonJs: require is valid in this context
-				const armor = require(module) as Armor;
+				const armor = require(module) as Readonly<Armor>;
 				armors.push(armor);
 			} catch (e) {
 				print(`Module ${module.Name} could not be built: ${tostring(e)}`);
--- src/shared/features/inventory/data/weapons/index.ts
+++ src/shared/features/inventory/data/weapons/index.ts
@@ -3,15 +3,15 @@ import { Weapon } from '../../types/weapon';
 
 // Flags
 let isInitialized: boolean = false;
-const weapons: Weapon[] = [];
+const weapons: Readonly<Weapon>[] = [];
 
-export function getWeapons(): Weapon[] {
+export function getWeapons(): ReadonlyArray<Readonly<Weapon>> {
 	if (!isInitialized) {
 		const modules = getDescendantsOfType(script, 'ModuleScript');
 		for (const module of modules) {
 			try {
 				// biome-ignore lint/style/noCommonJs: require is valid in this context
-				const weapon = require(module) as Weapon;
+				const weapon = require(module) as Readonly<Weapon>;
 				weapons.push(weapon);
 			} catch (e) {
 				print(`Module ${module.Name} could not be built: ${tostring(e)}`);
