8f80b2bab44825382916f265ad367a5379237ba7
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-08-03 21:04:47 -0500

    feat(DataService): added player unloading

--- src/server/features/datastore/services/data_service.ts
+++ src/server/features/datastore/services/data_service.ts
@@ -2,7 +2,8 @@ import { OnStart, Service } from '@flamework/core';
 import { Players } from '@rbxts/services';
 import { PlayerService } from 'server/features/player/services/player_service';
 import { PlayerSaveData } from '../types/schemas/inventory';
-import ProfileStore, { ProfileStoreProfile } from '../utils/profile_store';
+import ProfileStore from '../utils/profile_store';
+import type { ProfileStoreProfile } from '../utils/profile_store/types';
 
 const PROFILE_TEMPLATE: PlayerSaveData = {
 	equipped: {},
@@ -22,13 +23,15 @@ export class DataService implements OnStart {
 		this.playerService.addPlayerAddedCallback((player) => {
 			this.loadProfile(player);
 		});
+		this.playerService.addPlayerLeavingCallback((player) => {
+			this.unloadProfile(player);
+		});
 	}
 
 	public loadProfile(player: Player): void {
 		const profile = PLAYER_STORE.StartSessionAsync(tostring(player.UserId), {
 			Cancel: () => player.Parent !== Players,
 		});
-		print(profile);
 
 		if (!profile) return player.Kick('Failed to load your data');
 
@@ -42,6 +45,13 @@ export class DataService implements OnStart {
 			this.profiles.set(player, profile);
 			print(`Profile loaded for ${player.DisplayName}!`);
 		} else {
+			profile.EndSession();
+		}
+	}
+
+	public unloadProfile(player: Player): void {
+		const profile = this.profiles.get(player);
+		if (profile) {
 			profile.EndSession();
 		}
 	}
