2a80455f679534377d4a4633a6515ce2aa3ffcad
Brandon Kong <95515854+brandon-kong@users.noreply.github.com>
2025-08-04 22:52:30 -0500

    feat: added server dash connection

--- src/server/features/combat/services/combat_service.ts
+++ src/server/features/combat/services/combat_service.ts
@@ -1,4 +1,11 @@
 import { OnStart, Service } from '@flamework/core';
+import { ServerEvents } from 'server/signals/networking/events';
 
 @Service()
-export class CombatService {}
+export class CombatService implements OnStart {
+	public onStart(): void {
+		ServerEvents.dash.connect((player) => {
+			print('Dash on server', player);
+		});
+	}
+}
